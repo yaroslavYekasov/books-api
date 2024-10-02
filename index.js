036b06a1aaee97d51a4a219a9d600851314d3e44
fngpr <fngpr.56@gmail.com>
2024-10-02 19:05:53 +0300

    Created the node_modules/ folder and update package.json #188303032

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
